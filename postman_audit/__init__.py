2ea66020dea3f852ee4a52de1aea2b757cc9a51e
Taylor Daugherty <mdaugherty6@gmail.com>
2017-08-27 23:41:28 -0400

    finalized testing

--- postman_audit/__init__.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def main():
-    """Entry point to the application"""
-    r = requests.get('https://api.github.com/events')
-    print(r.text)
--- tests/test_audit.py
+++ tests/test_audit.py
@@ -1,25 +1,49 @@
-from unittest import TestCase
 from postman_audit.audit import Auditor
 from postman_audit.postman_client import PostmanClient
 
+import httpretty
+import json
+import os
 
-class AuditTest(TestCase):
-    def test_audit_initialize(self):
-        client = PostmanClient("key")
-        auditor = Auditor(client)
-        auditor.initialize()
-        self.assertTrue(auditor.collections.count() is not 0)
 
-    def test_audit_headers(self):
-        client = PostmanClient("key")
-        auditor = Auditor(client)
-        auditor.initialize()
-        auditor.audit_headers()
-        self.assertTrue(True)
+def setup_function(function):
+    httpretty.enable()
+    register_collections_meta()
+    register_collections()
 
-    def test_audit_prerequest(self):
-        client = PostmanClient("key")
-        auditor = Auditor(client)
-        auditor.initialize()
-        auditor.audit_prerequest()
-        self.assertTrue(True)
+
+def register_collections_meta():
+    collections_file = os.path.join(os.path.dirname(__file__), "data/collections.json")
+    with open(collections_file) as f:
+        collections_data = f.read()
+    httpretty.register_uri(httpretty.GET, PostmanClient.BASE_URL + "/collections", body=collections_data, status=201,
+                           content_type='application/json')
+
+
+def register_collections():
+    path = os.path.join(os.path.dirname(__file__), "data/collections")
+    files = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    for collection_file in files:
+        with open(collection_file) as f:
+            raw = f.read()
+            data = json.loads(raw)
+            httpretty.register_uri(httpretty.GET,
+                                   PostmanClient.BASE_URL + "/collections/631643-%s" % data['collection']['info']['_postman_id'],
+                                   body=raw, status=201, content_type='application/json')
+
+
+def teardown_function(function):
+    httpretty.disable()
+
+
+def test_audit_initialize():
+    client = PostmanClient("key")
+    auditor = Auditor(client, delay=0)
+    assert len(auditor.collections) is 3
+
+
+def test_audit_headers():
+    client = PostmanClient("key")
+    auditor = Auditor(client, delay=0)
+    auditor.audit_headers()
+    assert True
